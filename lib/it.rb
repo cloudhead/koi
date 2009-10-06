a3b1a2a8b9fdacc070ade44764a18c03f7e0acc0
cloudhead <self@cloudhead.net>
2009-10-06 11:04:37 -0400

    init

---
+++ lib/it.rb
@@ -0,0 +1,128 @@
+
+require 'yaml'
+require 'mutter'
+require 'fileutils'
+
+module It
+
+  Path = {root: "#{Dir.pwd}/.it", db: "#{Dir.pwd}/.it/database.yml"}
+  
+  def self.init
+    unless init?
+      FileUtils.mkdir Path[:root]
+    end
+  end
+
+  def self.init?
+    File.exist? Path[:root]
+  end
+  
+  def self.version
+    File.read(File.join(File.dirname(__FILE__), '..', 'VERSION')).strip
+  end
+  
+  class Command
+    Commands = ["init", "add", "list", "tag", "done", "remove", "float", "sink"]
+    Initializers = ["init", "add"]
+
+    def initialize cmd, param = nil, args = [], options
+      @command, @param, @args = cmd, param, args
+      @db = if File.exist? Path[:db]
+        YAML.load_file(Path[:db])
+      else
+        []
+      end
+      @mut = Mutter.new(blue: '#', underline: "''", cyan: '@@').clear(:default)
+    end
+    
+    def run
+      if Commands.include? @command
+        if It.init? or Initializers.include? @command
+          if send(@command, *@param, *@args)
+            save
+          else
+            abort "error running #@command"
+          end
+        else
+           abort "'it' is not initialized here, please run `it init`"
+        end
+      else
+        abort "#{@command} is not a valid command."
+      end
+    end
+    
+    def init
+      unless It.init
+        err "'it' has already been initialized here"
+      end; true
+    end
+    
+    def list index = -1
+      @db.each do |e|
+        out "#[#{index += 1}]# ''#{e[:title]}'' @@#{e[:tags].join(' ')}@@" unless e[:status] == :removed
+      end
+    end
+    
+    def out obj
+      if obj.is_a? Hash 
+        puts "#{obj[:title]} : #{obj[:status]}"
+      else
+        @mut.say obj.to_s
+      end
+    end
+    
+    def add entry
+      It.init
+      @db << Entity.new({title: entry})    
+    end
+    
+    def tag entry, tags
+      obj = @db.find {|e| e[:title].include? entry}
+      obj[:tags] << tags
+    end
+    
+    def done entry
+      obj = @db.find {|e| e[:title].include? entry}
+      obj[:status] = :complete
+      obj[:completed_at] = Time.now
+    end
+    
+    def save
+      File.open(Path[:db], 'w') {|f| f.write @db.to_yaml} 
+    end
+    
+    def remove entry
+      obj = @db.find {|e| e[:title].include? entry}
+      obj[:status] = :removed
+    end
+
+    def err str
+      $stderr.puts str
+    end
+  end
+  
+  class Entity
+    attr_accessor :data
+
+    def initialize data = []
+      @data = data
+      @data[:status] = :fresh
+      @data[:created_at] = Time.now
+      @data[:tags] = []
+    end
+
+    def to_yaml *args, &blk
+      @data.to_yaml *args, &blk
+    end
+    
+    def [] key
+      @data[key]
+    end
+
+    def []= key, val
+      @data[key] = val
+    end
+  end
+end
+
+
